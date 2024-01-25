5f955a9e88b4aeda6c9127aa6cb8468f2051ae91
Chris Bille <cbille1@icloud.com>
2024-01-24 19:30:46 -0500

    Initial Commit

---
+++ PayTab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PayTab
+//
+//  Created by Christopher Bille on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PayTab/PayTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayTabApp.swift
+//  PayTab
+//
+//  Created by Christopher Bille on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PayTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
