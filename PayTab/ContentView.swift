bdb2ac8ba1b52e31ed819f6db7ba074830656187
Chris Bille <cbille1@icloud.com>
2024-03-05 20:53:27 -0500

    Added total

--- PayTab/ContentView.swift
+++ PayTab/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tipAmount = 0.0
+    @State private var totalAmount = 0.0
     
     var body: some View {
         HStack {
@@ -17,8 +18,13 @@ struct ContentView: View {
         .padding()
         
         VStack {
-            Text("Enter tip amount")
-            TextField("Enter tip amount", value: $tipAmount, format: .number)
+            Text("Total")
+            TextField("Enter total", value: $totalAmount, format: .number)
+                .border(.secondary)
+                .padding()
+                .keyboardType(.decimalPad)
+            Text("Tip")
+            TextField("Enter tip", value: $tipAmount, format: .number)
                 .border(.secondary)
                 .padding()
                 .keyboardType(.decimalPad)
