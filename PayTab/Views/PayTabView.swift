ffa227914ba17d3fa0e5f70a6e79a993c39adeac
Chris Bille <cbille1@icloud.com>
2024-04-18 21:32:29 -0400

    Update removing keyboard

--- PayTab/Views/PayTabView.swift
+++ PayTab/Views/PayTabView.swift
@@ -5,66 +5,76 @@ struct PayTabView: View {
     @FocusState private var totalIsFocused: Bool
     
     var body: some View {
-        HStack {
-            Text("PayTab").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        }
-        .padding()
-        
-        VStack(alignment: .leading) {
-            LabeledContent {
-                TextField("Enter Total", value: $payTabViewModel.totalAmount, format: .currency(code: "USD"))
-                    .keyboardType(.decimalPad)
-                    .focused($totalIsFocused)
-            } label: {
-                Text("Total:")
-            }
-            .padding()
+        ScrollView {
             HStack {
-                Text("Tip:")
-                Text("\(String(format: "%.0f", payTabViewModel.tipAmount))%")
+                Text("PayTab").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             }
             .padding()
-            Slider(value: $payTabViewModel.tipAmount, in: 0...100, step: 5)
-                .padding()
-        }
-        .padding()
-        .textFieldStyle(.roundedBorder)
-        
-        Button("Calculate Total", systemImage: "arrow.up") {
-            payTabViewModel.totalAmountWithTip = payTabViewModel.calculateTotalwithTip()
-            payTabViewModel.renderTotalAmountWithTip = true
-            totalIsFocused = false
-        }
-        .buttonStyle(.bordered)
-        
-        if payTabViewModel.renderTotalAmountWithTip {
+            
             VStack(alignment: .leading) {
-                HStack {
-                    Text("Total With Tip:")
-                    if payTabViewModel.totalAmountWithTip == 0.00 {
-                        Text("$\(String(format: "%.0f", payTabViewModel.totalAmountWithTip))")
-                            .padding()
-                    } else {
-                        Text("$\(String(format: "%.2f", payTabViewModel.totalAmountWithTip))")
-                            .padding()
-                    }
+                LabeledContent {
+                    TextField("Enter Total", value: $payTabViewModel.totalAmount, format: .currency(code: "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($totalIsFocused)
+                        .toolbar {
+                            ToolbarItemGroup(placement: .keyboard) {
+                                Spacer()
+                                Button("Done") {
+                                    totalIsFocused = false
+                                }
+                            }
+                        }
+                } label: {
+                    Text("Total:")
                 }
-            }
-        } else {
-            Text("")
                 .padding()
-        }
-        
-        if payTabViewModel.renderTotalAmountWithTip {
-            Button("Clear", systemImage: "clear") {
-                payTabViewModel.totalAmount = 0.0
-                payTabViewModel.tipAmount = 0.0
-                payTabViewModel.totalAmountWithTip = 0
-                payTabViewModel.renderTotalAmountWithTip = false
+                HStack {
+                    Text("Tip:")
+                    Text("\(String(format: "%.0f", payTabViewModel.tipAmount))%")
+                }
+                .padding()
+                Slider(value: $payTabViewModel.tipAmount, in: 0...100, step: 5)
+                    .padding()
+            }
+            .padding()
+            .textFieldStyle(.roundedBorder)
+            
+            Button("Calculate Total", systemImage: "arrow.up") {
+                payTabViewModel.totalAmountWithTip = payTabViewModel.calculateTotalwithTip()
+                payTabViewModel.renderTotalAmountWithTip = true
+                totalIsFocused = false
             }
             .buttonStyle(.bordered)
+            
+            if payTabViewModel.renderTotalAmountWithTip {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Total With Tip:")
+                        if payTabViewModel.totalAmountWithTip == 0.00 {
+                            Text("$\(String(format: "%.0f", payTabViewModel.totalAmountWithTip))")
+                                .padding()
+                        } else {
+                            Text("$\(String(format: "%.2f", payTabViewModel.totalAmountWithTip))")
+                                .padding()
+                        }
+                    }
+                }
+            } else {
+                Text("")
+                    .padding()
+            }
+            
+            if payTabViewModel.renderTotalAmountWithTip {
+                Button("Clear", systemImage: "clear") {
+                    payTabViewModel.totalAmount = 0.0
+                    payTabViewModel.tipAmount = 0.0
+                    payTabViewModel.totalAmountWithTip = 0
+                    payTabViewModel.renderTotalAmountWithTip = false
+                }
+                .buttonStyle(.bordered)
+            }
+            Spacer()
         }
-        Spacer()
     }
 }
 
