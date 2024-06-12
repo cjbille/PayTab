1b19566df576704126826dfbaf58dab90dcf9e09
Chris Bille <cbille1@icloud.com>
2024-06-11 20:23:46 -0400

    Add more unit tests

--- PayTabTests/PayTabViewModelTest.swift
+++ PayTabTests/PayTabViewModelTest.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class PayTabViewModelTest: XCTestCase {
     
-    func testCalculateTotalwithTip() {
+    func testCalculateTotalwithTipShouldReturnTotalWithTip() {
         let payTabViewModel = PayTabViewModel()
         payTabViewModel.totalAmount = 10.0
         payTabViewModel.tipAmount = 20.0
@@ -11,4 +11,13 @@ final class PayTabViewModelTest: XCTestCase {
         let totalWithTipExpected = 12.0
         XCTAssertEqual(totalWithTipActual, totalWithTipExpected)
     }
+    
+    func testTipShouldReturnAmountToTip() {
+        let payTabViewModel = PayTabViewModel()
+        payTabViewModel.totalAmount = 10.0
+        payTabViewModel.totalAmountWithTip = 12.0
+        let amountToTipActual = payTabViewModel.tip()
+        let amountToTipExpected = 2.0
+        XCTAssertEqual(amountToTipActual, amountToTipExpected)
+    }	
 }
