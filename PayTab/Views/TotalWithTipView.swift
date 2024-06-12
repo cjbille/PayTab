c4c432c77905ef2b486aecdcc2b62142ae22731b
Chris Bille <cbille1@icloud.com>
2024-06-11 20:04:43 -0400

    Added more SwiftUI views

---
+++ PayTab/Views/TotalWithTipView.swift
@@ -0,0 +1,18 @@
+//
+//  TotalWithTipView.swift
+//  PayTab
+//
+//  Created by Christopher Bille on 5/25/24.
+//
+
+import SwiftUI
+
+struct TotalWithTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TotalWithTipView()
+}
